3aa204203fc493daebf73ef372319aa163e88d40
Archiebladess <florentinagita11@gmail.com>
2025-08-21 16:23:15 +0700

    edit new

--- sort/sort.r
+++ sort/sort.r
@@ -1,1 +1,6 @@
-library(read.csv)
+library(read.r)
+sample <- read.csv("C:/Users/FLORENTINA REGITA/Downloads/data-analysis-competition-2025/sample_submission.csv")
+head(sample)
+test_gpt <- read.csv("C:/Users/FLORENTINA REGITA/Downloads/data-analysis-competition-2025/Test/gpt.csv")
+head(test_gpt)
+test_claude <- 
